715db3bc5f55d3dbe830cfa0d6d9a2652633065b
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-06-07 20:38:58 -0300

    Update practica02.py

--- practica02.py
+++ practica02.py
@@ -138,3 +138,19 @@ if kilo_w > 700:
 
 #Salida
 print('El valor a pagar es: $', (kilo_w * precio_kilow))
+
+print('----------------------\n')
+print('Consigna 7:\n')
+
+#entradas:
+t = float(input('Ingrese temperatura: '))
+p = int(input('Ingrese 1 para convertir de ºF a ºC o 2 para convertir de ºC a ºF: '))
+while p != 1 and p != 2:
+  p = int(input('No ha ingresado una opción correcta! Ingrese 1 para convertir de ºF a ºC o 2 para convertir de ºC a ºF: '))
+
+if p == 1:
+  c = 5/9*(t-32)
+  print('Convirtiendo de ºF a ºC.... Resultado:', c, 'ºC')
+else:
+  c = 32+(9*t/5)
+  print('Convirtiendo de ºC a ºF.... Resultado:', c, 'ºF')
