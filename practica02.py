80b9b65d41fe33a166304b4f77daae13f16f8302
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-06-07 10:40:50 -0300

    Update practica02.py

--- practica02.py
+++ practica02.py
@@ -37,3 +37,58 @@ else:
 
 print('----------------------\n')
 print('Consigna 3:\n')
+
+#entradas
+
+l1 = float(input('Ingrese dimensión de lado 1: '))
+l2 = float(input('Ingrese dimensión de lado 2: '))
+l3 = float(input('Ingrese dimensión de lado 3: '))
+
+#cálculo diagonales
+d1 = ((l1**2) + (l2**2))**0.5
+d2 = ((l1**2) + (l3**2))**0.5
+d3 = ((l2**2) + (l3**2))**0.5
+d_mayor = max(d1, d2, d3)
+
+#salida
+if d_mayor == d1:
+  print('La diagonal mayor es compuesta por el lado 1 y el lado 2')
+else:
+  if d_mayor == d2:
+    print('La diagonall mayor es compuesta por el lado 1 y lado 3')
+  else:
+    print('La diagonal mayor es la compuesta por el lado 2 y el lado 3')    
+
+print('Mide: ', d_mayor)
+
+"""
+
+print('----------------------\n')
+print('Consigna 4:\n')
+#entradas
+
+l1 = float(input('Ingrese dimensión de lado 1: '))
+l2 = float(input('Ingrese dimensión de lado 2: '))
+l3 = float(input('Ingrese dimensión de lado 3: '))
+
+#cálculo diagonales
+d1 = ( (l1**2) + (l2**2) )**0.5
+d2 = ( (l1**2) + (l3**2) )**0.5
+d3 = ( (l2**2) + (l3**2) )**0.5
+d_mayor = max(d1, d2, d3)
+
+#salida
+if d_mayor == d1:
+  print('La diagonal mayor es compuesta por el lado 1 y el lado 2')
+else:
+  if d_mayor == d2:
+    print('La diagonall mayor es compuesta por el lado 1 y lado 3')
+  else:
+    print('La diagonal mayor es la compuesta por el lado 2 y el lado 3')    
+
+print('Mide: ', d_mayor)
+
+"""
+
+print('----------------------\n')
+print('Consigna 4:\n')
