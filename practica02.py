8e88c2d20daf57d20ec345898f830ca8dc1f23a4
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-06-08 08:50:45 -0300

    Update practica02.py

--- practica02.py
+++ practica02.py
@@ -182,3 +182,62 @@ else:
     print('El alumno 1 tiene una mayor calificacion con: ', cal_final_1)
   else:
     print('El alumno 2 tiene una mayor calificacion con: ', cal_final_2)
+
+
+print('----------------------\n')
+print('Consigna 9:\n')
+
+#emtradas
+cal1_est = float(input('Ingrese primer calificación: '))
+cal2_est = float(input('Ingrese segunda calificación: '))
+cal3_est = float(input('Ingrese tercer calificación: '))
+
+#proceso y salida
+print('La menor calificación del estudiante es:', min(cal1_est, cal2_est, cal3_est))
+print('La mayor calificación del estudiante es:', max(cal1_est, cal2_est, cal3_est))
+
+print('----------------------\n')
+print('Consigna 10:\n')
+
+#emtradas
+l1 = float(input('Ingrese longitud de lado 1 del triángulo: '))
+l2 = float(input('Ingrese longitud de lado 2 del triángulo: '))
+l3 = float(input('Ingrese longitud de lado 3 del triángulo: '))
+
+#proceso y salida
+if l1 == l2 and l1 == l3:
+  print('El triángulo formado es equilatero.')
+else:
+  if l1 != l2 and l1 != l3 and l2 != l3:
+    print('El tirángulo formado es escaleno.')
+  else:
+    print('El triángulo formado es isóseles.')
+    
+print('----------------------\n')
+print('Consigna 11:\n')
+
+#emtradas
+aaaa = int(input('Ingrese el año: '))
+
+#proceso y salida
+if aaaa % 400 == 0:
+  print('El año es biciesto.')
+else:
+  if aaaa % 4 == 0 and aaaa % 100 != 0:
+    print('El año es biciesto.')
+  else:
+    print ('El año no es biciesto.')
+
+print('----------------------\n')
+print('Consigna 12:\n')
+
+#emtradas
+n1 = int(input('Ingrese un número entre 0 y 10: '))
+while n1 < 0 or n1 > 10:
+  n1 = int(input('ERROR. Ingrese un número entre 0 y 10: '))
+
+#proceso y salida
+if n1 % 2 == 0:
+  print('El numero es par')
+else:
+  print('El numero es impar')
