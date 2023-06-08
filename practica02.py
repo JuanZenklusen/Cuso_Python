bc695e11e0b17823594540f69f1a5fc4aa8dcd9c
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-06-07 23:04:12 -0300

    Update practica02.py

--- practica02.py
+++ practica02.py
@@ -154,3 +154,31 @@ if p == 1:
 else:
   c = 32+(9*t/5)
   print('Convirtiendo de ºC a ºF.... Resultado:', c, 'ºF')
+
+print('----------------------\n')
+print('Consigna 8:\n')
+
+"""Dadas las tres calificaciones de dos estudiantes. La calificación final de cada uno
+es la suma de sus dos mejores calificaciones. Muestre un mensaje que indique cual
+estudiante (1 o 2) tiene la mayor calificación final."""
+
+#entradas:
+cal_a_1 = float(input('Ingrese primer calificacion del estudiante 1: '))
+cal_b_1 = float(input('Ingrese segunda calificacion del estudiante 1: '))
+cal_c_1 = float(input('Ingrese tercera calificacion del estudiante 1: '))
+cal_a_2 = float(input('Ingrese primer calificacion del estudiante 2: '))
+cal_b_2 = float(input('Ingrese segunda calificacion del estudiante 2: '))
+cal_c_2 = float(input('Ingrese tercera calificacion del estudiante 2: '))
+
+#proceso
+cal_final_1 = (cal_a_1 + cal_b_1 + cal_c_1) - min(cal_a_1, cal_b_1, cal_c_1)
+cal_final_2 = (cal_a_2 + cal_b_2 + cal_c_2) - min(cal_a_2, cal_b_2, cal_c_2)
+
+#salida
+if cal_final_1 == cal_final_2:
+  print('Ambos tienen la misma calificación')
+else:
+  if cal_final_1 > cal_final_2:
+    print('El alumno 1 tiene una mayor calificacion con: ', cal_final_1)
+  else:
+    print('El alumno 2 tiene una mayor calificacion con: ', cal_final_2)
