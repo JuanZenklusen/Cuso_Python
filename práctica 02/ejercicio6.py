8d772cf5d18e408404fc87854289399a06ebf525
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-06-13 18:16:47 -0300

    actualizacion

--- práctica 02/ejercicio6.py
+++ práctica 02/ejercicio6.py
@@ -6,7 +6,10 @@ precio_kilow = float(input('Ingrese el precio del Kw/h: '))
 
 #proceso
 if kilo_w > 700:
-  precio_kilow = precio_kilow * 1.05
+  precio_kilow_700 = precio_kilow * 1.05
+  precio_final = (700*precio_kilow)+((kilo_w-700)*precio_kilow_700)
+else:
+  precio_final = kilo_w*precio_kilow
 
 #Salida
-print('El valor a pagar es: $', (kilo_w * precio_kilow))
+print('El valor a pagar es: $', precio_final)
