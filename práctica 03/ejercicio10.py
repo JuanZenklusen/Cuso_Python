ea1c745b411999ddf5dd07df99e88d565d374992
JuanZenklusen <juani1812@gmail.com>
2023-06-22 17:11:17 -0300

    actualizar

--- práctica 03/ejercicio10.py
+++ práctica 03/ejercicio10.py
@@ -2,12 +2,12 @@ numero = int(input("Ingrese un número entero: "))
 resul = True
 
 if numero < 2:
-    resul = False
+   resul = False
 else:
-    for i in range(2, int(numero ** 0.5) + 1):
-        if numero % i == 0:
-            resul = False
-            break
+   for i in range(2, int(numero ** 0.5) + 1):
+      if numero % i == 0:
+        resul = False
+        break
 
 if resul:
   print('El numero es primo')
