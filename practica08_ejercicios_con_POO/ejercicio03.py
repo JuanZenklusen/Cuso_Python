803be7e0c1d29d7315fc0b326db047151a846f5e
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-07-20 07:43:02 -0300

    Finalizado ejercicio03.py

--- practica08_ejercicios_con_POO/ejercicio03.py
+++ practica08_ejercicios_con_POO/ejercicio03.py
@@ -9,11 +9,17 @@ class Estudiante:
     self.notas = notas
 
   def Promedios(self):
-    resultado = round(sum(self.notas)/len(self.notas),2)
-    return f'Promedio de notas del alumno {self.nombre}: {resultado}'
+    resultado = round(sum(self.notas)/len(self.notas),1)
+    return resultado
+
+  def Aprobar(self):
+    if self.Promedios() > 6.0:
+      return f'Aprobado'
+    else:
+      return f'Reprobado'
 
 estudiante1 = Estudiante('Juan Zenklusen', 20, [10,7,5])
-estudiante2 = Estudiante('José Luis Boidi', 35, [10,10,10])
+estudiante2 = Estudiante('José Luis Boidi', 35, [2,5,6])
 
-print(estudiante1.Promedios())
-print(estudiante2.Promedios())
+print(f'El estudiante {estudiante1.nombre} tiene de promedio {estudiante1.Promedios()}. El curso ha sido {estudiante1.Aprobar()}')
+print(f'El estudiante {estudiante2.nombre} tiene de promedio {estudiante2.Promedios()}. El curso ha sido {estudiante2.Aprobar()}')
