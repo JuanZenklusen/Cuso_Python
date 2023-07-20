674a25aba2a302a8a0b20bfc8d2268cd19120e68
JuanZenklusen <101181838+JuanZenklusen@users.noreply.github.com>
2023-07-19 22:29:21 -0300

    arreglo de funcion promedios. Falta terminar

--- practica08_ejercicios_con_POO/ejercicio03.py
+++ practica08_ejercicios_con_POO/ejercicio03.py
@@ -1,17 +1,19 @@
-'''Crea una clase Estudiante que tenga atributos como nombre, edad y notas (una lista de calificaciones). Añade métodos para calcular el promedio de las notas y para determinar si el estudiante ha aprobado o no (promedio mayor o igual a 60).'''
+'''Crea una clase Estudiante que tenga atributos como nombre, edad y notas (una lista de calificaciones). 
+Añade métodos para calcular el promedio de las notas y para determinar si el estudiante ha aprobado o no 
+(promedio mayor o igual a 60).'''
 
 class Estudiante:
   def __init__(self, nombre, edad, notas):
     self.nombre = nombre
     self.edad = edad
-    self.notas = []
+    self.notas = notas
 
-  def promedios(self):
-    resultado = sum(self.notas)/len(self.notas)
+  def Promedios(self):
+    resultado = round(sum(self.notas)/len(self.notas),2)
     return f'Promedio de notas del alumno {self.nombre}: {resultado}'
 
-estudiante1 = Estudiante('Juan', 'Zenklusen', [10,7,5])
-estudiante2 = Estudiante('José Luis', 'Alberini', [10,10,10])
+estudiante1 = Estudiante('Juan Zenklusen', 20, [10,7,5])
+estudiante2 = Estudiante('José Luis Boidi', 35, [10,10,10])
 
-print(estudiante1.promedios())
-print(estudiante2.promedios())
+print(estudiante1.Promedios())
+print(estudiante2.Promedios())
